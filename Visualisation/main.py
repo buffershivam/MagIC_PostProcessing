18815466335782af6a632e41cb9f1537d45c67db
Shivam Lal <shivam07.lal@gmail.com>
2025-10-27 22:39:52 +0530

    Update main.py
    Added sample code lines to check functioning of plot_generator

--- Visualisation/main.py
+++ Visualisation/main.py
@@ -6,3 +6,10 @@ f.read_stream_G()   #reads the binary G file
 
 #Print the parameters
 f.print_parameters()
+
+#Create plots of the G File
+p = plot_generator.GeneratePlots(f)
+
+p.generate_plot(prop="entropy",plot_type="Eq")
+p.generate_plot(prop="vphi",plot_type="Ortho")
+p.generate_plot(prop="vphi",plot_type="Mol")
