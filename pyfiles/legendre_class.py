cd1c4ea3c71b9b5840ad54fd982e0ebca4f4222b
sidmish18011997 <sidmish1801@gmail.com>
2025-10-28 15:46:58 +0530

    started working on cluster files

---
+++ pyfiles/legendre_class.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.ndimage import map_coordinates
+from scipy.interpolate import interp1d
+import sys
+import matplotlib.colors as mcolors
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import Normalize
+from matplotlib import cm
+import plotly.graph_objects as go
+from scipy.special import lpmv
+
+
